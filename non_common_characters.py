5f123e2e6d4dce1e7ad0c72b36224fd0af9b2e86
21p31a04g2 <134603637+21p31a04g2@users.noreply.github.com>
2023-06-06 14:45:25 +0530

    s1=input().lower() s2=input().lower() s1=set(s1) s2=set(s2) s1.discard("") s2.discard("") a=s1.symmetric_difference(s2) l=[] for i in a:     if i!="" and i.isalpha():         l.append(i)         l.sort() print("".join(l))

---
+++ non_common_characters.py
@@ -0,0 +1,14 @@
+s1=input().lower()
+s2=input().lower()
+s1=set(s1)
+s2=set(s2)
+s1.discard("")
+s2.discard("")
+a=s1.symmetric_difference(s2)
+l=[]
+for i in a:
+    if i!="" and i.isalpha():
+        l.append(i)
+        l.sort()
+print("".join(l))
+        
